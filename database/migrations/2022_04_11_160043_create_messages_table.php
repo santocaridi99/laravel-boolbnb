0f30f2860300d36e21ad446b1419c1332403aa53
santocaridi99 <santocaridi99@gmail.com>
2022-04-11 18:03:20 +0200

    adds Message Model and Migration

---
+++ database/migrations/2022_04_11_160043_create_messages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('lastname');
+            $table->string('email');
+            $table->text('content');
+            $table->string('attachment');
+            $table->foreignId('apartment_id')
+                ->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
