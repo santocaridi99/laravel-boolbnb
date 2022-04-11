59ac75def95aa11c2bf6e88d45206113d18e4c40
Samuel Scozzari <s_scozzari@libero.it>
2022-04-11 22:23:23 +0200

    Adds relations in sponsorship (model)

--- app/Sponsorship.php
+++ app/Sponsorship.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sponsorship extends Model
 {
-    //
+    public function apartments()
+    {
+        return $this->belongsToMany('App\Apartment');
+    }
 }
