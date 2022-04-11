9841e3755c3f9028da676538630f88e882701eaa
Samuel Scozzari <s_scozzari@libero.it>
2022-04-11 22:23:52 +0200

    Adds relations in Tag (model)

--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    public function apartments()
+    {
+        return $this->belongsToMany('App\Apartment');
+    }
 }
