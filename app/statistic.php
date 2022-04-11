babc15135c6b2c9dd05379bc88e34d15da9059b8
Samuel Scozzari <s_scozzari@libero.it>
2022-04-11 22:23:39 +0200

    Adds relations in statistic (model)

--- app/statistic.php
+++ app/statistic.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class statistic extends Model
 {
-    //
+    public function apartment()
+    {
+        return $this->belongsTo('App\Apartment');
+    }
 }
