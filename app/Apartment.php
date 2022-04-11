543dafcea3021e0ea660a4c0f851321c04eeeced
Samuel Scozzari <s_scozzari@libero.it>
2022-04-11 22:22:28 +0200

    Adds relations in apartment (model)

--- app/Apartment.php
+++ app/Apartment.php
@@ -6,5 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class Apartment extends Model
 {
-    //
+
+    //Collegamenti di cui fa parte
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag');
+    }
+
+    public function sponsorships()
+    {
+        return $this->belongsToMany('App\Sponsorship');
+    }
+
+    //Collegamenti che possiede
+
+    public function statistics()
+    {
+        return $this->hasMany('App\Statistic');
+    }
+
+    public function images()
+    {
+        return $this->hasMany('App\Image');
+    }
+
+    public function messages()
+    {
+        return $this->hasMany('App\Message');
+    }
 }
