f0b157ef3d88c62f3d711276a753b32e92a20e7f
lucreziabisciaio <lucreziabisciaio@gmail.com>
2022-04-12 18:24:09 +0200

    adds message.index routes button

--- resources/js/pages/apartments/Show.vue
+++ resources/js/pages/apartments/Show.vue
@@ -1,16 +1,18 @@
 <template>
-  <div>
+    <div>
       <!-- dettagli appartamenti -->
       <h1>Dettagli appartamento</h1>
+        <router-link
+          class="btn btn-info"
+          :to="{ name: 'messages.index' }"
+        >
+          Contatta l'Host
+        </router-link>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
