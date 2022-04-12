1f6c0383a84c8838eafa934d0392b296325227ee
lucreziabisciaio <lucreziabisciaio@gmail.com>
2022-04-12 18:22:41 +0200

    creates apartment card

---
+++ resources/js/components/ApartmentCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="card" style="width: 18rem;">
+        <img src="" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Apartment title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <!-- button -->
+            <router-link
+                class="btn btn-info"
+                :to="{ name: 'apartments.show' }"
+            >
+                Vedi dettaglio appartamenti
+            </router-link>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
