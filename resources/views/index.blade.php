f813332fcc3c225773784c987d5f2e43fcf5d7bc
lucreziabisciaio <lucreziabisciaio@gmail.com>
2022-04-12 17:32:52 +0200

    adds script and style

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,13 +2,19 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
-    <div id="app">
-        <script src="{{asset('js/vue.js')}}"></script>
-    </div>
+    <div id="app"></div>
+
+    <script src="{{ asset('js/vue.js') }}"></script>
 </body>
+
 </html>
