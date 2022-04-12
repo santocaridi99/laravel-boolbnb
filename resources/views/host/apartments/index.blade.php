75d2a2d4d93b460c39183ffdb9648e5aef94fc4d
KCorigliano <corigliano98@hotmail.com>
2022-04-12 18:37:27 +0200

    fix route show

--- resources/views/host/apartments/index.blade.php
+++ resources/views/host/apartments/index.blade.php
@@ -17,7 +17,7 @@
                     <a class="p-2" href="{{ route('host.apartments.create') }}">Crea un nuovo appartamento</a>
                     @foreach ($apartments as $apartment)
                         <div class="card p-2">
-                            <p class="text-uppercase"><a href="{{ route('host.apartments.show', $apartment->id) }}">{{ $apartment->title}}</a></p>
+                            <p class="text-uppercase"><a href="{{ route('host.apartments.show', $apartment->slug) }}">{{ $apartment->title}}</a></p>
                             @if ($apartment->cover)
                                 <img class="img-fluid" src="{{ $apartment->cover }}" alt="" class="post-img">
                             @else
