200051e6aff7aeb84a5f1b1a1c6286ddf04bcc64
lucreziabisciaio <lucreziabisciaio@gmail.com>
2022-04-12 18:24:31 +0200

    creates simple footer layout

--- resources/js/components/TheFooter.vue
+++ resources/js/components/TheFooter.vue
@@ -1,5 +1,9 @@
 <template>
-  <div></div>
+  <div>
+    <footer class="bg-dark fixed-bottom">
+      <p class="text-white text-center">Ciao sono un footer</p>
+    </footer>
+  </div>
 </template>
 
 <script>
