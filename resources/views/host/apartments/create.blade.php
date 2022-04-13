da26401577a1d62a3dcd3230243d212fa74d907a
Samuel Scozzari <s_scozzari@libero.it>
2022-04-13 19:42:59 +0200

    removes old script, adds script (coordinates.js)

--- resources/views/host/apartments/create.blade.php
+++ resources/views/host/apartments/create.blade.php
@@ -20,7 +20,7 @@
 
 <body>
   <div class="container mt-5">
-    <form class="row g-3" action="{{ route('host.apartments.store') }}" method="post" enctype="multipart/form-data">
+    <form class="row g-3" id="formid" action="{{ route('host.apartments.store') }}" method="post" enctype="multipart/form-data">
 
       @csrf
 
@@ -193,48 +193,11 @@
 
       <div class="form-group">
         <a href="{{ route('host.apartments.index') }}" class="btn btn-secondary">Annulla</a>
-        <button type="submit" class="btn btn-success" onmouseover="beforeSubmit()">Salva appartamento</button>
+        <a class="btn btn-success" onmousedown="beforeSubmit()">Salva appartamento</a>
         {{-- <a id="geocodeBtn" class="btn btn-success">Geolocalizza</a>  --}}
       </div>
     </form>
   </div>
-
-  <script> 
-
-    //Here’s what our corresponding javascript would look like: 
-      beforeSubmit = function () { 
-        let city = document.getElementById('geoCity').value;
-        let address = document.getElementById('geoAddress').value;
-        let civic = document.getElementById('geoCnum').value;
-        
-        let completeAddress = address + ' ' + civic + ' ' + city;
-
-        var geocodeOptions = { 
-            query: completeAddress, 
-            key: 'm5upOBKh2ugQazsa72XgmQ7fFAuUxA9y' 
-        }; 
-        // Look up the geocode of the given address 
-        tt.services.geocode(geocodeOptions).then(function (geocodeRes) { 
-            console.log(geocodeRes);
-            var reverseOptions = { 
-                position: geocodeRes.results[0].position, 
-                key: 'm5upOBKh2ugQazsa72XgmQ7fFAuUxA9y' 
-            } 
-            console.log(geocodeRes.results[0].position.lat)
-            console.log(geocodeRes.results[0].position.lng)
-            
-            let lat = geocodeRes.results[0].position.lat;
-            let lng = geocodeRes.results[0].position.lng;
-
-            document.getElementById('longitudeHtml').value = lng;
-            document.getElementById('latitudeHtml').value = lat;
-    
-            // with our geocode, do a reverse look-up to get our original address back 
-            /* tt.services.reverseGeocode(reverseOptions).then(function (reverseRes) { 
-                console.log(reverseRes); 
-            });  */
-        }); 
-    }; 
-  </script> 
+  <script type="text/javascript" src="{{ URL::asset('js/script/coordinates.js') }}"></script> 
 </body>
 </html>
