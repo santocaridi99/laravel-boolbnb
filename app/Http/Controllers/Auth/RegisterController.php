2735f11b155dd138d72c73ea7dd1e7843c3ff8c5
santocaridi99 <santocaridi99@gmail.com>
2022-04-13 13:15:52 +0200

    adds  lastnme and birthdate in User Model (Complete the previous task)

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,6 +53,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            // aggiunto alla validazione lastname e birthdate
             'lastname' => ['required', 'string', 'max:255'],
             'birth_date' => ['required', 'date'],
         ]);
