7e9b19850ca3ca9b4db1e7e4eaa759ad200711d0
KCorigliano <corigliano98@hotmail.com>
2022-04-15 17:27:14 +0200

    navbar fix routes front/back- office

--- resources/js/components/TheNavbar.vue
+++ resources/js/components/TheNavbar.vue
@@ -45,13 +45,27 @@
               <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
               <button class="btn btn-outline-light" type="submit">Search</button>
             </form>
-          </ul>
+        </ul>
 
           <!-- login/register area -->
           <ul class="navbar-nav ml-auto">
-            <li class="nav-item d-flex">
-              <a class="btn btn-outline-light mx-2" href="/login">Login</a>
-              <a class="btn btn-outline-light" href="/register">Register</a>
+            <li class="nav-item dropdown" v-if="!user">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Login / Register 
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="/login">Login</a></li>
+                <li><a class="dropdown-item" href="/register">Register</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown" v-else>
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                {{ user.name }}
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="/host">Logout</a></li>
+                <li><a class="dropdown-item" href="/host/apartments">I miei appartamenti</a></li>
+              </ul>
             </li>
           </ul>
         </div>
@@ -61,9 +75,50 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  
-}
+  data() {
+    return {
+      routes: [],
+      user: null,
+    };
+  },
+  methods: {
+    fetchUser() {
+      // recuperiamo l'utente loggato tramite api
+      axios
+        .get("/api/user")
+        .then((resp) => {
+          // Nel caso in cui sia loggato, riceviamo i dettagli dell'utente
+          // li salviamo in una variabile locale.
+          this.user = resp.data;
+          // All'interno del localStorage, salviamo i dettagli dell'utente
+          localStorage.setItem("user", JSON.stringify(resp.data));
+          // Per comunicare in tempo reale che l'utente loggato è cambiato,
+          // lanciamo un evento custom su window
+          window.dispatchEvent(new CustomEvent("storedUserChanged"));
+        })
+        .catch((er) => {
+          // Entriamo nel catch SOLO se l'utente non è loggato e il server ci ritorna
+          // un codice diverso da 200. Se non siamo loggati abbiamo un 401.
+          console.error("Utente non loggato");
+          // Nel caso di errore, cancelliamo i dettagli dell'utente dal localStorage
+          localStorage.removeItem("user");
+          // Per comunicare in tempo reale che l'utente loggato è cambiato,
+          // lanciamo un evento custom su window
+          window.dispatchEvent(new CustomEvent("storedUserChanged"));
+        });
+    },
+  },
+  mounted() {
+    this.routes = this.$router
+      .getRoutes()
+      .filter((route) => !!route.meta.linkText);
+    this.fetchUser();
+    console.log(this.routes);
+  },
+};
 </script>
 
 <style>
--- resources/views/host/apartments/show.blade.php
+++ resources/views/host/apartments/show.blade.php
@@ -4,28 +4,28 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-8 card p-2">
-        <img class="img-fluid mb-2" src="{{ asset('storage/' . $apartment->cover) }}" alt="" class="post-img">
-        <p>Immagini relative all'appartamento:</p>
+        <div class="d-flex justify-content-between">
+          <span class="text-uppercase"><a href="{{ route('host.apartments.index') }}">< Torna indietro</a></span>
+          <span class="text-uppercase"><a href="{{ route('host.apartments.edit', $apartment->slug) }}">Modifica</a></span>
+        </div>
+        <img class="img-fluid mb-2 rounded" src="{{ asset('storage/' . $apartment->cover) }}" alt="" class="post-img">
+        <h3>Immagini relative all'appartamento:</h3>
         <div class="row p-2">
           @foreach ($apartment->images as $item)
             <div class="col-md-2">
-              <img class="img-fluid" src="{{ asset('image/' . $item->images) }}" alt="" class="post-img">
+              <img class="img-fluid rounded" src="{{ asset('image/' . $item->images) }}" alt="" class="post-img">
             </div>
           @endforeach
         </div>
-        <h2>Titolo</h2>
-        <h3>{{$apartment->title}}</h3>
-        <h2>Indirizzo:</h2>
+        <h3>Titolo</h3>
+        <h4>{{$apartment->title}}</h4>
+        <h3>Indirizzo:</h3>
         <div class="d-flex">
           <p class="me-2">{{$apartment->post_code}}</p>
           <p class="me-2">{{$apartment->province}}</p>
           <p class="me-2">{{$apartment->city}}</p>
           <p class="me-2">{{$apartment->address}}</p>
           <p class="me-2">{{$apartment->street_number}}</p>
-        </div>
-        <div class="d-flex justify-content-between">
-          <span class="text-uppercase""><a href="{{ route('host.apartments.index') }}">< Torna indietro</a></span>
-          <span class="text-uppercase"><a href="{{ route('host.apartments.edit', $apartment->slug) }}">Modifica</a></span>
         </div>
       </div>
     </div>
