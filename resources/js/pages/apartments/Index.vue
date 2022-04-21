944036423aadb4af3a4bc05552d2a4faa5b2dfea
Samuel Scozzari <s_scozzari@libero.it>
2022-04-21 13:11:53 +0200

    adds callback to server apartments

--- resources/js/pages/apartments/Index.vue
+++ resources/js/pages/apartments/Index.vue
@@ -7,15 +7,29 @@
         <div
             class="d-flex d-flex justify-content-center align-items-center flex-wrap"
         >
-            <ApartmentCard></ApartmentCard>
+            <!-- <ApartmentCard></ApartmentCard>
             <div v-if="singleLocation !== null">
                 {{ singleLocation.address.freeformAddress }}
+            </div> -->
+
+            <div class="my-card-cont">
+                <div v-for="apartment of apartments" :key="apartment.id"></div>
             </div>
+            <nav>
+                <ul class="my-pages">
+                    <li v-for="page in pagination.last_page" :key="page">
+                        <a class="my-page-link" @click="decodePostJson(page)">
+                            {{ page }}
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </div>
 </template>
 
 <script>
+import axios from "axios";
 import ApartmentCard from "../../components/ApartmentCard.vue";
 export default {
     components: {
@@ -25,7 +39,31 @@ export default {
         singleLocation: Object,
     },
     data() {
-        return {};
+        return {
+            apartments: [],
+            pagination: {},
+        };
+    },
+    methods: {
+        // assegnavo valore di default alla pagina 1
+        async decodeApartmentsJson(page = 1) {
+            if (page < 1) {
+                page = 1;
+            }
+            if (page > this.pagination.last_page) {
+                page = this.pagination.last_page;
+            }
+            /*  axios.get("/api/posts").then((resp) => {
+                this.postToPrint = resp.data;
+            }); */
+            const resp = await axios.get("/api/apartments?page=" + page);
+            this.pagination = resp.data;
+            console.log(resp.data.data);
+            this.apartments = resp.data.data;
+        },
+    },
+    mounted() {
+        this.decodeApartmentsJson();
     },
 };
 </script>
