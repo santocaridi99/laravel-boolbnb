f9b35e12c5854e06efa30a75ee8ef8a0cc7a88a0
Samuel Scozzari <s_scozzari@libero.it>
2022-04-21 10:13:44 +0200

    Creates emit on "luoghi" array in App.vue

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,51 +1,53 @@
 <template>
-  <div>
-    <TheNavbar></TheNavbar>
+    <div>
+        <TheNavbar @clicked="onClickChild"></TheNavbar>
 
-    <router-view></router-view>
+        <router-view></router-view>
 
-    <TheFooter></TheFooter>
-
-
-  </div>
+        <TheFooter></TheFooter>
+    </div>
 </template>
 
 <script>
-import axios from "axios";
+/* import axios from "axios"; */
 import TheNavbar from "../components/TheNavbar.vue";
 import TheFooter from "../components/TheFooter.vue";
 
 export default {
-  components: { 
-    TheNavbar,
-    TheFooter,
-  },
-  data() {
-    return {
-      user: null,
-    }
-  },
-  methods: {
-    getStoredUser() {
-      const storedUser = localStorage.getItem("user");
-      if (storedUser) {
-        this.user = JSON.parse(storedUser);
-      } else {
-        this.user = null;
-      }
+    components: {
+        TheNavbar,
+        TheFooter,
     },
-  },
-  mounted() {
-     this.getStoredUser();
+    data() {
+        return {
+            user: null,
+            callbackLuoghi: [],
+        };
+    },
+    methods: {
+        onClickChild(value) {
+            this.callbackLuoghi = value;
+            console.log(value);
+            console.log(this.callbackLuoghi, "questi sono i luoghi");
+        },
 
-    window.addEventListener('storedUserChanged', () => {
-      this.getStoredUser();
-    });
-  }
+        getStoredUser() {
+            const storedUser = localStorage.getItem("user");
+            if (storedUser) {
+                this.user = JSON.parse(storedUser);
+            } else {
+                this.user = null;
+            }
+        },
+    },
+    mounted() {
+        this.getStoredUser();
 
-}
+        window.addEventListener("storedUserChanged", () => {
+            this.getStoredUser();
+        });
+    },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
