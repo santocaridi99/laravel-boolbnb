cb71f35ff6244e4ef8c078b4d90264e74d21d038
lucreziabisciaio <lucreziabisciaio@gmail.com>
2022-05-02 17:46:23 +0200

    adds trashbin to index

--- resources/views/host/apartments/index.blade.php
+++ resources/views/host/apartments/index.blade.php
@@ -13,9 +13,10 @@
             
             <h1 class="text-start text-white fw-bold f_45">I miei alloggi</h1>
 
-            @if(count($apartments))
-                <div class="d-flex justify-content-center mb-3 mt_50">
-                    {{-- create --}}
+            
+            <div class="d-flex justify-content-center mb-3 mt_50">
+                {{-- create --}}
+                @if(count($apartments))
                     <div class="create_box">
                         <a class="create_svg" href="{{ route('host.apartments.create') }}">
                             <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="39" height="39" fill="none">
@@ -55,36 +56,36 @@
                         </a>
                         <div class="create_slide text-white">Aggiungi un nuovo appartamento</div>
                     </div>
+                @endif
                     
-                    {{-- trash bin --}}
-                    <a class="ms-auto" href="{{ route('host.apartments.deletedApartments')}}" title="Cestino">
-                        <span class="text-white align-middle bin_text f_18 op_7">Cestino</span>
-                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none">
-                            <style>
-                                @keyframes rotate-tr {
-                                    0% {
-                                        transform: rotate(0)
-                                    }
-
-                                    to {
-                                        transform: rotate(20deg)
-                                    }
+                {{-- trash bin --}}
+                <a class="ms-auto" href="{{ route('host.apartments.deletedApartments')}}" title="Cestino">
+                    <span class="text-white align-middle bin_text f_18 op_7">Cestino</span>
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none">
+                        <style>
+                            @keyframes rotate-tr {
+                                0% {
+                                    transform: rotate(0)
                                 }
-                            </style>
-                            <path fill="#FFF"
-                                d="M16.773 10.083a.75.75 0 00-1.49-.166l1.49.166zm-1.535 7.027l.745.083-.745-.083zm-6.198 0l-.745.083.745-.083zm-.045-7.193a.75.75 0 00-1.49.166l1.49-.166zm5.249 7.333h-4.21v1.5h4.21v-1.5zm1.038-7.333l-.79 7.11 1.491.166.79-7.11-1.49-.166zm-5.497 7.11l-.79-7.11-1.49.166.79 7.11 1.49-.165zm.249.223a.25.25 0 01-.249-.222l-1.49.165a1.75 1.75 0 001.739 1.557v-1.5zm4.21 1.5c.892 0 1.64-.67 1.74-1.557l-1.492-.165a.25.25 0 01-.248.222v1.5z" />
-                            <path fill="#EDB900" fill-rule="evenodd"
-                                d="M11 6a1 1 0 00-1 1v.25H7a.75.75 0 000 1.5h10a.75.75 0 000-1.5h-3V7a1 1 0 00-1-1h-2z"
-                                clip-rule="evenodd"
-                                style="animation:rotate-tr 1s cubic-bezier(1,-.28,.01,1.13) infinite alternate-reverse both;transform-origin:right center" />
-                        </svg>
-                    </a>
-                </div>
-            @endif
+
+                                to {
+                                    transform: rotate(20deg)
+                                }
+                            }
+                        </style>
+                        <path fill="#FFF"
+                            d="M16.773 10.083a.75.75 0 00-1.49-.166l1.49.166zm-1.535 7.027l.745.083-.745-.083zm-6.198 0l-.745.083.745-.083zm-.045-7.193a.75.75 0 00-1.49.166l1.49-.166zm5.249 7.333h-4.21v1.5h4.21v-1.5zm1.038-7.333l-.79 7.11 1.491.166.79-7.11-1.49-.166zm-5.497 7.11l-.79-7.11-1.49.166.79 7.11 1.49-.165zm.249.223a.25.25 0 01-.249-.222l-1.49.165a1.75 1.75 0 001.739 1.557v-1.5zm4.21 1.5c.892 0 1.64-.67 1.74-1.557l-1.492-.165a.25.25 0 01-.248.222v1.5z" />
+                        <path fill="#EDB900" fill-rule="evenodd"
+                            d="M11 6a1 1 0 00-1 1v.25H7a.75.75 0 000 1.5h10a.75.75 0 000-1.5h-3V7a1 1 0 00-1-1h-2z"
+                            clip-rule="evenodd"
+                            style="animation:rotate-tr 1s cubic-bezier(1,-.28,.01,1.13) infinite alternate-reverse both;transform-origin:right center" />
+                    </svg>
+                </a>
+            </div>
 
             {{-- APARTMENTS CARDS --}}
             @if(!count($apartments))
-                <div class="text-center create_empty my_300">
+                <div class="text-center create_empty my_190_280">
                     <h3 class="text-white mb-5 f_35 op_9">Non hai ancora caricato il tuo appartamento?</h3>
                     <a href="{{ route('host.apartments.create') }}" class="px-4 py-3 pink_custom text-white f_18">Crea ora il tuo primo annuncio!</a>
                 </div>
